2481159deaf6e7f38da9cd502f5023555cef2a07
esamjr <agildimasjr@gmail.com>
2020-08-18 13:01:11 +0700

    fifth

---
+++ fifth/main.go
@@ -0,0 +1,104 @@
+package main //Change it to main
+
+import "fmt"
+
+// Using Struct
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+	email     string
+	IsActive  bool
+}
+
+// Using Method For User
+func (user User) display() string {
+	result := fmt.Sprintf("Name : %s %s, Email : %s", user.FirstName, user.LastName, user.email)
+	return result
+}
+
+type Group struct {
+	Name        string
+	Admin       User
+	Users       []User
+	IsAvailable bool
+}
+
+// Using Method For Group
+func (group Group) displayGroup() {
+	fmt.Printf("Name Group : %s", group.Name)
+
+	fmt.Printf("\n")
+
+	fmt.Printf("Member Group : %d", len(group.Users))
+
+	fmt.Printf("\n")
+
+	for _, user := range group.Users {
+		fmt.Println("-", user.FirstName)
+	}
+}
+func main() {
+
+	// Method 1 Use Struct
+	user := User{
+		ID:        1,
+		FirstName: "Rudolf",
+		LastName:  "Darwin",
+		email:     "rudolf@darwin.com",
+		IsActive:  true,
+	}
+	result := user.display()
+	fmt.Println("With Method : ")
+	fmt.Println(result)
+
+	// Method 2 Use Struct
+	user2 := User{}
+	user2.ID = 2
+	user2.FirstName = "Emilia"
+	user2.LastName = "Parker"
+	user2.email = "emilia@parker.com"
+	user2.IsActive = true
+
+	// How to Output Struct With Method
+	result = user2.display()
+	fmt.Println("With Method : ")
+	fmt.Println(result)
+
+	//How to Output Display User With Function
+	display1 := displayUser(user)
+	display2 := displayUser(user2)
+	fmt.Println("With Function : ")
+	fmt.Println(display1)
+	fmt.Println(display2)
+
+	//How to Output Display Group With Method
+	users := []User{user, user2}
+	group := Group{"Gamer PlayGround", user, users, true}
+	fmt.Println("With Method : ")
+	group.displayGroup()
+
+	// How to Output Display Group With Function
+	users1 := []User{user, user2}
+	group1 := Group{"Gamer PlayGround 2", user, users1, true}
+	fmt.Println("With Function : ")
+	displayGroup(group1)
+
+}
+
+// function For Struct Group
+func displayGroup(group Group) {
+	fmt.Printf("Name : %s", group.Name)
+	fmt.Printf("\n")
+	fmt.Printf("Member Group : %d", len(group.Users))
+	fmt.Printf("\n")
+	for _, user := range group.Users {
+		fmt.Println("-", user.FirstName)
+	}
+}
+
+// Function For Struct User
+func displayUser(user User) string {
+	result := fmt.Sprintf("Name : %s %s, Email : %s", user.FirstName, user.LastName, user.email)
+	return result
+}
